f13c666d925a74487bd4f9d478ae489f32fe47fd
Imaad Uddin <imaaduddin27@gmail.com>
2025-01-05 02:10:50 -0600

    adding button functionaility

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,12 @@
+import { useRef } from "react";
+
 import Input from "./Input";
 
 export default function NewProject() {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
@@ -8,9 +14,9 @@ export default function NewProject() {
                 <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input label="Title" />
-                <Input label="Description" textarea />
-                <Input label="Due Date" />
+                <Input ref={title} label="Title" />
+                <Input ref={description} label="Description" textarea />
+                <Input ref={dueDate} label="Due Date" />
             </div>
         </div>
     );
